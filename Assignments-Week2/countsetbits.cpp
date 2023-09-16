7b004987b5a5bd43c374025e7f97474af3b1c222
maddymundhra <41953646+maddymundhra@users.noreply.github.com>
2023-09-16 16:59:56 +0530

    Create countsetbits.cpp

---
+++ Assignments-Week2/countsetbits.cpp
@@ -0,0 +1,25 @@
+// Count all set bits in a number
+// Example: n=10
+// Binary of n= 1010
+// Ans: setbits = 2;
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int bit;
+    int count=0;
+    while(n>0)
+    {
+        bit=(n&1);
+        if(bit==1)
+        {
+            count++;
+        }
+        n=n>>1;
+    }
+    cout<<count<<endl;
+}
